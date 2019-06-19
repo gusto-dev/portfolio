ea313767b9c2dc6bce6fc6a06d5304a7d02fb083
Gusto <lhg0616@gmail.com>
2019-06-19 19:58:34 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.99f2d19daab696d50089aae055947718.js"
+  "/portfolio/precache-manifest.2c1b32d84da9637d17d3f1941f684f81.js"
 );
 
 self.addEventListener('message', (event) => {
