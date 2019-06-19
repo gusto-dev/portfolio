fdd13cffab9d9afff9a169d7dcc618a8b2e8b518
Gusto <lhg0616@gmail.com>
2019-06-19 19:59:03 +0900

    portfolio init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
-import TrackVisibility from 'react-on-screen';
-import Users from './Users';
-import Posts from './Posts';
-import Comments from './Comments';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TrackVisibility offset={500} once>
-          <Users />
-        </TrackVisibility>
-        <TrackVisibility offset={300} once>
-          <Posts />
-        </TrackVisibility>
-        <TrackVisibility offset={450} once>
-          <Comments />
-        </TrackVisibility>
+        hello
       </div>
     );
   }
