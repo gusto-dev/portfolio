21b6e185f3abe26ca9033cbf69295599e724418c
Gusto <lhg0616@gmail.com>
2019-06-19 23:38:16 +0900

    added css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import HEADER from "./components/HEADER";
+import LISTS from "./components/LISTS";
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      header:{title:'WEB', sub:'World Wid Web!'}
+    }
+  }
   render() {
     return (
       <div className="App">
-        hello
+        <HEADER title={this.state.header.title}
+          sub={this.state.header.sub}></HEADER>
+        <LISTS></LISTS>
       </div>
     );
   }
