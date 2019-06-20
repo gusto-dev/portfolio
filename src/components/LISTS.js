367b17d261927b7f3d08623549ae4548c8cea850
gusto-dev <lhg0616@gmail.com>
2019-06-20 18:50:35 +0900

    updated components/list.js

--- src/components/LISTS.js
+++ src/components/LISTS.js
@@ -6,7 +6,18 @@ class LISTS extends Component{
       <section className="lists">
         <p>* 시간이 지남에 따라 제가 작업한 내용에서 변화가 있을 수 있습니다. ( 최대한 제외 하고 있습니다. )</p>
         <div className="table-wrap">
-          sdf
+          <div className="group-item tit">
+            <span>프로젝트명</span>
+            <span>정보</span>
+            <span>기여도</span>
+            <span>사이트</span>
+          </div>
+          <div className="group-item">
+            <span>뭐</span>
+            <span>뭐</span>
+            <span>뭐</span>
+            <span>뭐</span>
+          </div>
         </div>
       </section>
     );
