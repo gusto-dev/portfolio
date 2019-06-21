efe0144ae3f7a15be65de2b2100c82fa593b4558
Gusto <lhg0616@gmail.com>
2019-06-21 17:24:01 +0900

    Updates

--- precache-manifest.b67384b3724fda66f556ce62f804366d.js
+++ precache-manifest.b67384b3724fda66f556ce62f804366d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d56f143747f00e9bafc5e1e5cf2f9b46",
+    "revision": "983c7caf61c2f8aba581bb4312fddc01",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "d344b0aa12dd9ebf4cff",
-    "url": "/portfolio/static/css/main.758e6c2a.chunk.css"
+    "revision": "da7e9b6db0e58d005bba",
+    "url": "/portfolio/static/css/main.9836f56b.chunk.css"
   },
   {
     "revision": "b6aafa07ca16cd706e06",
     "url": "/portfolio/static/js/2.2752fb64.chunk.js"
   },
   {
-    "revision": "d344b0aa12dd9ebf4cff",
-    "url": "/portfolio/static/js/main.4a8a466b.chunk.js"
+    "revision": "da7e9b6db0e58d005bba",
+    "url": "/portfolio/static/js/main.475727a0.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3462ee3b5f86f2062296819442cecd04.js"
+  "/portfolio/precache-manifest.b67384b3724fda66f556ce62f804366d.js"
 );
 
 self.addEventListener('message', (event) => {
