0f529ce49a4e0d3d2704cd40cab358276a0749ac
gusto-dev <lhg0616@gmail.com>
2020-07-26 17:16:38 +0900

    commit test

--- src/components/LISTS.js
+++ src/components/LISTS.js
@@ -4,7 +4,7 @@ class LISTS extends Component{
   render(){
     return (
       <section className="lists">
-        <p>* 시간이 지남에 따라 제가 작업한 내용에서 변화가 있을 수 있습니다. ( 최대한 제외 하고 있습니다. )</p>
+        <p>* 시간이 지남에 따라 제가 작업한 내용에서 변화가 있을 수 있습니다. ( 최대한 제외 하고 있습니다. ) </p>
         <div className="table-wrap">
           <div className="group-item tit">
             <span>프로젝트명</span>
